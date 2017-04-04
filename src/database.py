9e19c39f9d3cd824286daa7701cf85a3fbca61a4
Giuliano <morgan@anouk.oats.inaf.it>
2017-04-04 17:08:27 +0200

    Create new files for functions and add a new function for fits creation . Needs tests

--- src/database.py
+++ src/database.py
@@ -1,11 +1,27 @@
-#!/usr/bin/python
-"""
-This is an example of python script for runnung many times the 
-code, varying one parameter existing in one module .h file 
-Here is the vegetation albedo in vegetation.h
+#!/usr/bin/env python
+#
+# This is an example of python script for runnung many times the
+# code, varying one parameter existing in one module .h file
+# Here is the vegetation albedo in vegetation.h
+#
+#   GM created
+#   GT 29/3/17 some small improvements
+#
+#
 
-GT 29/3/17 some small improvements
-"""
+'''
+    Input paramters of the computational kernel:
+    * simtype,
+    * version,
+    * simulation number
+    
+    
+    Values of Pressure, Raddi Obliquities and Eccentricities to cycle
+    
+    WARNING, 0.001 e 0.005 of pressure are not working
+    Pressures are epressed in times the Earth value
+
+'''
 
 import sys
 import os
@@ -13,93 +29,9 @@ import shutil
 from posix import system
 import logging
 
+from fitslib import create_FITS
+from workarea import *
 
-def copyall (src, dest):
-    src_files = os.listdir(src)
-    for file_name in src_files:
-        full_file_name = os.path.join(src, file_name)
-        if (os.path.isfile(full_file_name)):
-            shutil.copy(full_file_name, dest)
-    return
-
-def collective_move (src, dest):
-    src_files = os.listdir(src)
-    for file_name in src_files:
-        full_file_name = os.path.join(src, file_name)
-        if (os.path.isfile(full_file_name)):
-            shutil.move(full_file_name, dest)
-    return
-
-def make_work_area (_p,_ecc,_obl,_dist):
-# multiple output directory preparation
-# here we need to catch exeptions (necessary on parallel runs)
-    os.makedirs("RisultatiMultipli")
-    os.makedirs("Database")
-    os.makedirs("LogFiles")
-    copyall("ModulesDef", "Src")
-    copyall("Std","Src")
-    if simtype=="VegPassive":
-        copyall(template_dir+"/VegPassive","Src")
-        copyall(template_dir+"/VegPassive/Modules/","Src")
-    elif simtype=="VegAlbedoFB":
-        copyall(template_dir+"/VegAlbedoFB", "Src")
-        copyall(template_dir+"/VegAlbedoFB/Modules/","Src")
-                # varying pressure on an EARTH-LIKE planet, using EARTH template
-    shutil.copy("Planets/EARTH.py","Src/PRESSURES.py")
-    shutil.copy("Planets/fo_earth_DMAP.dat","Src/fo_earth_DMAP.dat")
-
-'''
-    configuring planet parameter file
-    
-    PRESSURES.py is the template (here, EARTH.py)   GT: maybe better to call it PRESSURES.templ ?
-    PRESSUREScurr.py is what runEBM.py uses
-
-'''
-    fileout=open('Src/PRESSUREScurr.py',"w")
-        with open('Src/PRESSURES.py', 'r') as f:
-            for tl in f:
-                pl = tl
-                if 'ff=1.0' in tl:
-                    pl="ff=%f\n"%_p
-                if 'eccP=' in tl:
-                    pl='eccP=%f\n'%_ecc
-                if 'obliq=' in tl:
-                    pl='obliq=%f\n'%_obl
-                if 'smaP=' in tl:
-                        pl='smaP=%f\n'%_dist
-                fileout.write(pl)
-    fileout.close()
-    os.makedirs("Risultati")
-    return
-
-def archive_results(p,ecc,dist,obl):
-    shutil.copy("PRESSUREScurr.py","Risultati")
-    shutil.copy("parEBM.h","Risultati")
-    str="../RisultatiMultipli/Risultati_Press%5.3f_Ecc%4.2f_Dist%3.1f_Obl%5.3f"%(p,ecc,dist,obl)
-    os.mkdir(str)
-    str="../RisultatiMultipli/Risultati_Press%5.3f_Ecc%4.2f_Dist%3.1f_Obl%5.3f"%(p,ecc,dist,obl)
-    collective_move("Risultati",str)
-    return
-
-def archive_logs(p,ecc,dist,obl):
-    str="../LogFiles/log_Press%5.3f_Ecc%4.2f_Dist%3.1f_Obl%5.3f"%(p,ecc,dist,obl)
-    shutil.move("log",str)
-
-
-
-'''
-    Input paramters of the computational kernel:
-        * simtype, 
-        * version,
-        * simulation number
-    
-    
-    Values of Pressure, Raddi Obliquities and Eccentricities to cycle
-    
-    WARNING, 0.001 e 0.005 of pressure are not working
-    Pressures are epressed in times the Earth value
-'''
-    
 Pressures=[0.01, 0.1, 0.5, 1.0, 3.0, 5.0]
 Radii= [0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
 Obliquities = [0., 15., 23.43929, 30., 45.]
@@ -107,7 +39,19 @@ Eccentricities = [ 0.0, 0.01671022, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
 simtype="Std"
 version="1.1.03"
 number=1
+
+# Directories and Files
 template_dir="/work/Programming/Esoclimi/Devel"
+RisultatiMultipli = "RisultatiMultipli"
+Database = "Database"
+LogFiles = "LogFiles"
+Risultati ="Risultati"
+Src = "Src"
+
+
+fits_out_base = "ESTM1.1.01-"                       # base name of the FITs output
+fortran_run_result="year_lat_temp_last1.tlt"        # Data File name
+fits_param_file = "NONMELORICORDO"
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -116,7 +60,7 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 
-shutil.copytree(template_dir+"/CCM_RH60", "Src") # GT why here?
+
 
 
 
@@ -128,9 +72,7 @@ for ecc in Eccentricities:
             
                 make_work_area()
 
-
-
-                os.chdir("Src")
+                os.chdir(Src)
                 
                 #insert a log here
                 logging.info("%s",os.getcwd())
@@ -141,10 +83,12 @@ for ecc in Eccentricities:
                 #system(str)
 
                 #now we have results. Making .fits file (REQUIRES PYFIT)
-                os.chdir("Risultati")
+                #os.chdir("Risultati")
                 logging.info("%s","python ../../fits_map_temperature.py")
-                system("python ../../fits_map_temperature.py") # MUST BE INCLUDED IN THIS CODE AS A FUCTION
-
+                
+                #create_FITS(fortran_result_file,fits_out_base,fits_param_file)
+                
+                
                 for file in os.listdir("/mydir"):   #maybe not necessary if include the fits conveter
                     if file.endswith(".fits") and file.find("ESTM"):
                         shutil.copy(file,"../../Database")
@@ -167,5 +111,3 @@ for ecc in Eccentricities:
                 print "\n\n\n"
 
 
-if __name__ == '__main__':
-    main()
---
+++ src/fitslib.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+#  fitslib.py
+#  Esoclimi
+#
+#  Created by giuliano taffoni on 04/04/17.
+#  Copyright © 2017 Guliano Taffoni. All rights reserved.
+
+"""
+    
+    
+    create_FITS(in, out, param)
+    
+    
+    Create a FITS file [out] from output file [in] of the Fortran Code Esopianeti
+    The stracture of the FITS is given by the parameter file [param]
+    It requires pyfits, math and numpy
+
+   
+"""
+
+## this function parse the output file from fortran code
+def create_FITS(in_file,out_file,param_file):
+    import numpy as np
+    import pyfits
+    import math
+    
+    prihdr = pyfits.Header() # Create Header
+    with open(in_file, 'r') as f:
+        first_line = f.readline().rstrip().split()
+    #print "Creating fits with Ns, N=",first_line
+    N=int(math.floor(float(first_line[1])))
+    Ns=int(math.floor(float(first_line[0])))
+    prihdr['N']  = (N, 'number of latitude zones')
+    prihdr['Ns']  = (Ns, 'number of time steps per orbit')
+
+    lines = [line.rstrip('\n') for line in open(pfile)]
+    for line in lines:
+        if line:
+            words = line.split('!')
+            if words[0] == "COMMENT":
+                prihdr['COMMENT'] = words[2]
+            else:
+                if words[3]=="F":
+                    prihdr[words[0]]  = (float(words[1]),words[2])
+                elif words[3]=="I":
+                    prihdr[words[0]]  = (int(words[1]),words[2])
+                else:
+                    prihdr[words[0]]  = (words[1],words[2])
+            if words[0] == "DATE":
+                date=words[1]
+            if words[0] == "NUMBER":
+                number=words[1]
+
+    prihdu = pyfits.PrimaryHDU(header=prihdr)
+
+#print "Date of creation of map: ",date
+    out_file=out_file+date[0:2]+"."+date[2:4]+"."+date[4:8]+"-"+number+".fits"
+#print "Creating ",out_file
+
+    data=np.loadtxt(in_file, skiprows=1)    # Read data from input file
+    year, lat, temp = data.T
+    year = year - math.floor(year[0])       # This subtracts the integer part of "year"
+
+    c1=pyfits.Column(name='year', format='E', array=year)
+    c2=pyfits.Column(name='lat', format='E', array=lat)
+    c3=pyfits.Column(name='temp', format='E', array=temp)
+
+    cols=pyfits.ColDefs([c1,c2,c3])
+
+    tbhdu=pyfits.BinTableHDU.from_columns(cols)
+
+    thdulist = pyfits.HDUList([prihdu, tbhdu])
+
+    thdulist.writeto(out_file)
